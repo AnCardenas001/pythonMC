3d1389ffa7d2f583f90276aa27bde2356c7c812d
AnCardenas001 <paulaa.cardenash@konradlorenz.edu.co>
2023-09-04 21:01:20 -0500

    CifradoCaesar en python

--- CadenasDeCaracteres.py
+++
@@ -1,7 +0,0 @@
-# Definimos una varaible string con el nombre
-nombre = "Daniel"
-segundaLetra = nombre[1]
-
-print("La segunda letra del nombre es " + segundaLetra)
-
-print("El nombre tiene " + str(len(nombre)) + " letras")
